bc005e895792c0e23dfcdfca44c83f81924c3d7c
Jason Vinson <vinsonizer@gmail.com>
2019-04-28 05:00:23 -0400

    base worshipplanning login service

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
+const wp = require('./lib/worshipplanning')
+
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => res.send('Hello World'))
+app.get('/', (req, res) => wp.login(function (err, token) {
+  if (err) console.log(err)
+  else res.send(token)
+}))
 
 app.listen(PORT, () => console.log(`Example app listening on ${PORT}!`))
