73c932d3c5eda1fd97a557a10439aada4cca0b70
Jason Vinson <vinsonizer@gmail.com>
2019-04-29 09:09:54 -0400

    added test timeout setting till mocks in place

--- test/worshipplanning-spec.js
+++ test/worshipplanning-spec.js
@@ -12,6 +12,7 @@ describe('WP', function () {
 
   describe('getEvents', function () {
     it('should return events', function (done) {
+      this.timeout(3000) // temporary timeout till we add mocks
       wp.getEvents(function (err, data) {
         if (err) assert.fail('No data')
         else should.exist(data); done()
