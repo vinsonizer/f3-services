cc61087152d22ec0b8f4db07213b867712edb9f4
Jason Vinson <vinsonizer@gmail.com>
2022-03-24 18:04:13 -0400

    big cleanup

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
-const express = require('express')
-const app = express()
-const wp = require('./lib/worshipplanning')
-const slack = require('./lib/slack')
+const express = require("express");
+const app = express();
+const wp = require("./lib/worshipplanning");
+//const slack = require('./lib/slack')
 
-app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get("/", (req, res) => res.redirect("/api/events"));
 
-app.get('/api/heartbeat', (req, res) => res.send('Ok'))
+app.get("/api/heartbeat", (req, res) => res.send("Ok"));
 
-app.get('/api/events', (req, res) => wp.getEvents(function (err, events) {
-  if (err) {
-    console.log(err); res.send('Error: ' + err)
-  } else res.json(events)
-}))
+app.get("/api/events/", (req, res) =>
+  wp.getEvents(req.query.page || 1, function (err, events) {
+    if (err) {
+      console.log(err);
+      res.send("Error: " + err);
+    } else {
+      res.header("Content-Type", "application/json");
+      res.send(JSON.stringify(events, null, 4));
+    }
+  })
+);
 
-app.listen(PORT, () => console.log(`Example app listening on ${PORT}!`))
+app.listen(PORT, () => console.log(`Example app listening on ${PORT}!`));
